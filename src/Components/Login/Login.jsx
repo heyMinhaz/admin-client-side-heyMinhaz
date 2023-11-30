4bd30a003201313a57cbc4d5542ae168c376a0c7
Minhazul Abdin <hey_minhaz@Minhazuls-MacBook-Air.local>
2023-11-30 15:46:41 +0600

    end

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -15,7 +15,7 @@ import {
 import { AuthContext } from "../Authprovider/Authprovider";
 import Swal from "sweetalert2";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import app from "../../FireBase/Firebase.config";
+
 
 const Login = () => {
   const auth = getAuth(app);
